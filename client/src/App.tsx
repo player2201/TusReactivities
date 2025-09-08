eb4e3d08a8e1211c8e97cfe501f5c6ce87f4b0cb
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2025-09-08 12:22:01 +0300

    Apply .gitignore and remove ignored files from tracking

---
+++ client/src/App.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    fetch("https://localhost:5001/api/activities")
+      .then((response) => response.json())
+      .then((data) => setActivities(data));
+  }, []);
+  return (
+    <div>
+      <h3 className="app" style={{ color: "red" }}>
+        TusReactivities
+      </h3>
+      <ul>
+        {activities.map((activity) => (
+          <li key={activity.id}>{activity.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
