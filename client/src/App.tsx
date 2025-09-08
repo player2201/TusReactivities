540231eb866ab62445dcbd3531785c99cc1216a3
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2025-09-08 13:01:46 +0300

    Added and use Material UI in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { List, ListItem, ListItemText, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 
 function App() {
@@ -9,16 +10,16 @@ function App() {
       .then((data) => setActivities(data));
   }, []);
   return (
-    <div>
-      <h3 className="app" style={{ color: "red" }}>
-        TusReactivities
-      </h3>
-      <ul>
+    <>
+      <Typography variant="h3">TusReactivities</Typography>
+      <List>
         {activities.map((activity) => (
-          <li key={activity.id}>{activity.title}</li>
+          <ListItem key={activity.id}>
+            <ListItemText>{activity.title}</ListItemText>
+          </ListItem>
         ))}
-      </ul>
-    </div>
+      </List>
+    </>
   );
 }
 
