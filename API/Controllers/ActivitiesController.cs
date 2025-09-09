ee06ad33bc65df07883d245e3155ce82f14aa206
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2025-09-09 20:04:41 +0300

    Cleaning up ActivitiesController.cs

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -8,17 +8,17 @@ using Persistence;
 
 namespace API.Controllers;
 
-public class ActivitiesController(IMediator mediator) : BaseApiController
+public class ActivitiesController : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return await mediator.Send(new GetActivityList.Query());
+        return await Mediator.Send(new GetActivityList.Query());
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        return await mediator.Send(new GetActivityDetails.Query { Id = id });
+        return await Mediator.Send(new GetActivityDetails.Query { Id = id });
     }
 }
--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -5,5 +6,15 @@ namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BaseApiController : ControllerBase { }
+    public class BaseApiController : ControllerBase
+    {
+        private IMediator? _mediator;
+
+        // "??=" if the thing to the left is null, then set it to be whatever is to the right of the "="
+        //"??" check if the thing to the left is null
+        protected IMediator Mediator =>
+            _mediator ??=
+                HttpContext.RequestServices.GetService<IMediator>()
+                ?? throw new InvalidOperationException("IMediator service is unavailable");
+    }
 }
