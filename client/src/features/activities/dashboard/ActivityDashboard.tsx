3d26c5a16a85782ec958f8c24f8d4b38f016326d
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2025-09-13 13:02:05 +0300

    Created an Activity form component. No functionality yet.

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import { Grid } from "@mui/material";
 import ActivityList from "./ActivityList";
 import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
 
 type Props = {
   activities: Activity[];
@@ -27,6 +28,7 @@ export default function ActivityDashboard({
             cancelSelectActivity={cancelSelectActivity}
           />
         )}
+        <ActivityForm />
       </Grid>
     </Grid>
   );
