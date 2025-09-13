e0d4dfedca91131effb5ae53320fbd260307e8d8
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2025-09-13 11:40:09 +0300

    Created a card component for the activities.

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,8 +1,8 @@
-import { Container, CssBaseline } from "@mui/material";
+import { Box, Container, CssBaseline } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import NavBar from "./NavBar";
-import ActivityDashboard from "../../features/activities/ActivityDashboard";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -13,13 +13,13 @@ function App() {
       .then((response) => setActivities(response.data));
   }, []);
   return (
-    <>
+    <Box sx={{ backgroundColor: "#eeeeee" }}>
       <CssBaseline />
       <NavBar />
       <Container maxWidth="xl" sx={{ marginTop: 3 }}>
         <ActivityDashboard activities={activities} />
       </Container>
-    </>
+    </Box>
   );
 }
 
